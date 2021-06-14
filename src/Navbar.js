4ef1f8c8a306a864849f92cf8dc8e080487cc88d
Abdilahi khaliif Hassan <daarood11@hotmail.com>
2021-06-14 11:37:16 +0200

    Funkar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (  
+<nav className="narbar">
+
+    <h1>Do joBlog</h1>
+    <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" >New blog</a>
+    </div>
+</nav>
+
+    );
+}
+ 
+export default Navbar;
